5f0fd352ed26faf0333462df03d875ff99a7a897
Nivaash <nidishnivash@gmail.com>
2023-12-27 16:57:34 +0530

    done

---
+++ pro-tips/src/components/PageNotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <center>
+        <img style={{width:"100%",height:"85vh"}} src="https://i0.wp.com/learn.onemonth.com/wp-content/uploads/2017/08/1-10.png?fit=845%2C503&ssl=1" alt="" />
+      </center>
+    </div>
+  )
+}
+
+export default PageNotFound
